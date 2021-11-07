06aaa0bfc7f471ee349d245f6b9d2a3c6b40f114
LilleLink <simon_0_0_@live.se>
2021-11-07 16:03:52 +0100

    Fixes lab 1 for final deadline

--- lab1/lab1.hs
+++ lab1/lab1.hs
@@ -25,7 +25,6 @@ stepsPower _ k = k + 1
 
 power1 :: Integer -> Integer -> Integer
 power1 n k
-   | k == 0 = 1
    | k < 0 = error "power: negative argument"
    | otherwise = product myList where myList = [n | _ <- [1..k]]
 
@@ -44,13 +43,26 @@ power2 n k
     Tests:
     Mathematical tests to make sure the function outputs expected 
     values for supported inputs:
-    1. 0 to the power of any integer >0 should always be 0.
-    2. 1 to the power of any positive integer should always be 1.
-    3. Any integer raised to the power of 0 should always be 1.
+
+    This is to make sure that 0 to the power of an integer is 0.
+    1. 0 to the power of 2 should be 0. 
+
+    This is to make sure that an integer to the power of 0 is 1.
+    2. 2 raised to the power of 0 should be 1.
+
+    This is to make sure that 1 to the power of an integer is 1.
+    3. 1 to the power of 50 should be 1.
+
+    This is to test that the function supports very large numbers
+    4. 2 to the power of 100 should be 1267650600228229401496703205376.
+
+    These are to make sure that the function works for negative bases
+    5. -2 to the power of 3 should be -8.
+    6. -2 to the power of 4 should be 16.
+
     Program tests for making sure the functions handle unsupported types 
-    or values correctly:
-    4. Negative exponents should cause an error - Since its not supported
-    5. Functions should only take integers - to prevent type errors
+    7. Negative exponents should cause an error - Since its not supported
+    8. Functions should only take integers - to prevent type errors
  -}
 
 -- Tests that the power functions return the same value for some n, k. 
@@ -58,12 +70,14 @@ power2 n k
 prop_powers :: Integer -> Integer -> Bool
 prop_powers n k = (power n k == power1 n k) && (power n k == power2 n k)
 
--- Tests cases 1, 2, 3. Works only for n >= 0
-powerTest :: Integer -> Bool
-powerTest n = prop_powers n 0 && prop_powers 1 n && prop_powers 0 n
+-- Tests well behaved test cases.
+powerTest :: Bool
+powerTest = prop_powers 0 2 && prop_powers 2 0 
+   && prop_powers 1 50 && prop_powers 2 100
+   && prop_powers (-2) 3 && prop_powers (-2) 4
 
 -- Tests that the power functions returns the same value for some n, k.
 -- Takes absolute value of k to avoid errors.
 prop_powers' :: Integer -> Integer -> Bool
-prop_powers' n k = (power n k' == power1 n k') && (power n k' == power2 n k')
+prop_powers' n k = prop_powers n k'
    where k' = abs k
