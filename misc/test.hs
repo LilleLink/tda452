c9868a95efb406ec18d76a643692a574295068e3
LilleLink <simon_0_0_@live.se>
2021-12-16 12:25:00 +0100

    Hash func for chars

---
+++ misc/test.hs
@@ -0,0 +1,17 @@
+alphabet = ['A'..'Z']
+chars = "EASYQUTION"
+
+placeInAlphabet :: Char -> Int
+placeInAlphabet ch = pia ch alphabet 1
+    where
+        pia ch [] count = count
+        pia ch (c:cs) count | ch == c = count
+                            | otherwise = pia ch cs (count+1)
+
+hash :: Char -> Int
+hash c = 11* placeInAlphabet c `mod` 10
+
+main :: IO ()
+main = do
+    let hashes = zip chars (map hash chars)
+    print hashes
